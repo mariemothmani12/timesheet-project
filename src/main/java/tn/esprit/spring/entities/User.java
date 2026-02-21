4de062234131e67d9f529b684e02e5bdab35d0c2
othmanimariem <mariem.othmani@esprit.tn>
2026-02-21 21:07:03 +0100

    Chap4 – Last Name

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -71,8 +71,8 @@ public class User implements Serializable {
 	}
 
 	public String getLastName() {
-		return lastName;
-	}
+                return lastName + "-v1";
+        }
 	public void setLastName(String lName) {
 		this.lastName = lName;
 	}
